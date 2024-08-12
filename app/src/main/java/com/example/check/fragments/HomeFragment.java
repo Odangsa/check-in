26356ae159de9bf155f7f12700bb70ad40605ef5
oyeong011 <oyeong011@gmail.com>
2024-08-12 19:35:35 +0900

    각 프래그먼트 및 북비티아이 메인 화면 분리

--- app/src/main/java/com/example/check/fragments/HomeFragment.java
+++ app/src/main/java/com/example/check/fragments/HomeFragment.java
@@ -1,13 +1,14 @@
-package com.example.check;
+package com.example.check.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.widget.SearchView;
+
+import com.example.check.R;
 
 public class HomeFragment extends Fragment {
 
